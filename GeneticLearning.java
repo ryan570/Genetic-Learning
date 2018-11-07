0474cc138e6952a12d84d39ac2ef55f00ee63ad5
Ryan <brownryan570@gmail.com>
2018-11-07 00:13:16 -0500

    Add beginning

--- GeneticLearning.java
+++ GeneticLearning.java
@@ -4,8 +4,11 @@ import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.Light.Point;
 import javafx.scene.input.MouseEvent;
@@ -23,19 +26,15 @@ public class GeneticLearning extends Application {
     public static Point2D goal, start;
     private final Rectangle selection = new Rectangle();
     private final Point anchor = new Point();
+    private Button begin;
+    private Label generationLabel, stepLabel;
+    private Timeline timeline;
     
     @Override
     public void start(Stage primaryStage) {
         root = new Pane();
 
-        Scene scene = new Scene(root, 800, 400);
-
-        primaryStage.setTitle("Genetic Learning");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
         pop = new Population(100);
-        pop.display();
 
         goal = new Point2D(750, 200);
         Circle goalCircle = new Circle(goal.getX(), goal.getY(), 5);
@@ -45,22 +44,21 @@ public class GeneticLearning extends Application {
         Circle startCircle = new Circle(start.getX(), start.getY(), 5);
         startCircle.setFill(Color.BLACK);
 
-        Label generationLabel = new Label();
-        Label stepLabel = new Label();
+        begin = new Button("Start Evolution");
+        begin.setLayoutX(355);
+        begin.setOnAction(this::beginEvolution);
+
+        generationLabel = new Label();
+        stepLabel = new Label();
         stepLabel.setLayoutY(15);
 
-        root.getChildren().addAll(selection, goalCircle, startCircle, generationLabel, stepLabel);
+        root.getChildren().addAll(selection, begin, goalCircle, startCircle);
 
-        scene.setOnMousePressed(this::onMousePress);
-        scene.setOnMouseDragged(this::onDrag);
-        scene.setOnMouseReleased(this::onRelease);
-
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), e -> {
+        timeline = new Timeline(new KeyFrame(Duration.millis(10), e -> {
             if (!pop.allDead()) {
                 pop.update();
             } else {
                 pop.setMaxStep();
-
                 pop.evolve();
                 pop.mutate();
             }
@@ -68,11 +66,28 @@ public class GeneticLearning extends Application {
             stepLabel.setText(String.format("Step Count: %d", pop.maxStep));
         }));
         timeline.setCycleCount(Animation.INDEFINITE);
-        timeline.play();
+
+        Scene scene = new Scene(root, 800, 400);
+
+        scene.setOnMousePressed(this::onMousePress);
+        scene.setOnMouseDragged(this::onDrag);
+        scene.setOnMouseReleased(this::onRelease);
+
+        primaryStage.setTitle("Genetic Learning");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void beginEvolution(ActionEvent event) {
+        root.getChildren().addAll(generationLabel, stepLabel);
+        root.getChildren().remove(begin);
+
+        pop.display();
+        timeline.play();
     }
 
     private void onMousePress(MouseEvent event) {
